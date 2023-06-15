c6031ab4b7f671e9ca9c730b99e2e4e68ae778a6
root <walaasaad591@gmail.com>
2023-06-15 08:01:19 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-int main (void)
+
+/**
+ * main - Entry point
+ * 
+ * Description: 'puts program'
+ * @parameter: no parameters
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
+
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
