3cb883e051a0f3f3825b528ec62dc01bd186b804
root <walaasaad591@gmail.com>
2023-06-15 09:08:19 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,13 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Description: 'puts program'
- * @parameter: no parameters
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
