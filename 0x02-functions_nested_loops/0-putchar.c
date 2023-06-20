9335ca5f66903233037c5809b61cd54670befa86
root <walaasaad591@gmail.com>
2023-06-20 14:35:00 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+void _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'puts program'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char x[] = "_putchar\n";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(x[i]);
+}
+
+return (0);
+}
