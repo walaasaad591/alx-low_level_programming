87836a391a362244e8c9334cd3cbc65ea203105b
root <walaasaad591@gmail.com>
2023-06-20 14:44:42 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
+#include <main.h>
 
-
-void _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
