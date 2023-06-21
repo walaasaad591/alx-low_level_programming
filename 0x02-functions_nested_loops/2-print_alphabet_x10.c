ff3e3ccc74d9df3ad50c14a4a036d4ec84b97b1f
root <walaasaad591@gmail.com>
2023-06-20 18:24:12 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints the alphabet in lowercase, with new line.
  *
  * Description: 'puts program'
  *
