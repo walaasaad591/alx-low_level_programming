7eeb260b3110ace25f5c294c63ce18728bf317d8
root <walaasaad591@gmail.com>
2023-06-20 18:03:30 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet function all alphapet
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  *
  * Description: 'puts program'
  *
