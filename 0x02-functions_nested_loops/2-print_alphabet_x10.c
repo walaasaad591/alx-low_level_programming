567363300f5dbebd0fdd720aa56c9a2fcd58b3bb
root <walaasaad591@gmail.com>
2023-06-20 18:18:03 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints 10 times the alphabet,lowercase.
+ * print_alphabet - Prints 10 times the alphabet,lowercase xyzhhhhhhh.
  */
 void print_alphabet_x10(void)
 {
