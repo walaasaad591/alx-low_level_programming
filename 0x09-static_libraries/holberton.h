f11992d57be54ac03aa77ebcfa383d3031a33f2f
basma saadeldeen <https://github.com/basmasaadeldeen2023/zero_day.git>
2023-07-07 22:34:01 +0300

    hhv

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,6 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stddef.h>
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+/*copy list from task*/
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +23,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
