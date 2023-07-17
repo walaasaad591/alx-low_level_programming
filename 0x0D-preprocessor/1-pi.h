eb19fc8ad37ffd170d3ae4aac20845cf2dd19839
basma saadeldeen <https://github.com/basmasaadeldeen2023/zero_day.git>
2023-07-17 13:09:51 +0300

    djdjjd

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,5 +1,5 @@
-#ifndef _PI_
-#define _PI_
+#ifndef PI_H
+#define PI_H
 
 #define PI 3.14159265359
 
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file.
+ * main - preints the name of the file
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
